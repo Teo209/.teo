4c795e00015d7a8b1a729f5df43d3bb5745d0f0b
Teo209 <teo43212@gmail.com>
2026-09-12 15:52:57 +0300

    Added AST nodes

--- src/teo/ast/expression.py
+++ src/teo/ast/expression.py
@@ -1,16 +1,35 @@
 class Expression:
     pass
 
+
 # literal
 class Literal(Expression):
     def __init__(self, value) -> None:
         self.value = value
+    
+    def __repr__(self) -> str:
+        return f"Literal (value: {self.value})"
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Literal):
+            return NotImplemented
+        
+        return self.value == other.value
 
 
 # access variables
 class Variable(Expression):
     def __init__(self, name) -> None:
         self.name = name
+    
+    def __repr__(self) -> str:
+        return f"Variable (name: {self.name})"
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Variable):
+            return NotImplemented
+        
+        return self.name == other.name
 
 
 # binary
@@ -19,6 +38,18 @@ class BinaryOperation(Expression):
         self.left = left
         self.operator = operator
         self.right = right
+    
+    def __repr__(self) -> str:
+        return f"BinaryOperation (operation: {self.left} {self.operator} {self.right})"
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, BinaryOperation):
+            return NotImplemented
+        
+        return (
+                self.left == other.left and
+                self.operator == other.operator and
+                self.right == other.right)
 
 
 # unary
@@ -26,3 +57,14 @@ class UnaryOperation(Expression):
     def __init__(self, operator, right: Expression) -> None:
         self.operator = operator
         self.right = right
+    
+    def __repr__(self) -> str:
+        return f"UnaryOperation (operation: {self.operator}{self.right})"
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, UnaryOperation):
+            return NotImplemented
+        
+        return (
+            self.operator == other.operator and
+            self.right == other.right)
--- src/teo/ast/statement.py
+++ src/teo/ast/statement.py
@@ -8,14 +8,43 @@ class Statement:
 class ExpressionStatement(Statement):
     def __init__(self, expression: Expression) -> None:
         self.expression = expression
+    
+    def __repr__(self) -> str:
+        return f"ExpressionStatement (expression: {self.expression})"
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, ExpressionStatement):
+            return NotImplemented
+        
+        return self.expression == other.expression
 
 
 class Assignment(Statement):
     def __init__(self, name: str, value: Expression) -> None:
         self.name = name
         self.value = value
-
+    
+    def __repr__(self) -> str:
+        return f"Assignment ({self.name} = {self.value})"
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Assignment):
+            return NotImplemented
+        
+        return (
+            self.name == other.name and
+            self.value == other.value
+        )
 
 class ConsoleLog(Statement):
     def __init__(self, output: Expression) -> None:
         self.output = output
+    
+    def __repr__(self) -> str:
+        return f"ConsoleLog (console_log {self.output})"
+    
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, ConsoleLog):
+            return NotImplemented
+        
+        return self.output == other.output
